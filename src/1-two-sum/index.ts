19f122a4c57a86a1e3690942e2b82c70b357a9f5
Andriy <andriy.frankevych@gmail.com>
2023-03-15 12:27:17 +0100

    update twoSum return value to be empty if there is no match

--- src/1-two-sum/index.ts
+++ src/1-two-sum/index.ts
@@ -1,11 +1,9 @@
 export const twoSum = (nums: number[], target: number): number[] => {
   for(let i = 0; i <= nums.length - 1; i++) {
-    for (let j = i + 1; j <= nums.length; j++) {
-      if (nums[i] + nums[j] === target) {
-        return [i, j]
-      }
+    for (let j = i + 1; j <= nums.length - 1; j++) {
+      if (nums[i] + nums[j] === target) return [i, j];
     }
   }
 
-  throw new Error("Invalid input: provided array has no elements that sums up to target")
+  return [];
 };
